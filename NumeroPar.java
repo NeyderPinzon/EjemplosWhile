fdc93823364a61687bfbbfcaa053b735c083f121
Nicolas Pinzon <31898680+NeyderPinzon@users.noreply.github.com>
2017-10-16 13:37:56 -0300

    Numero Par con While

---
+++ NumeroPar.java
@@ -0,0 +1,29 @@
+package NumeroPar;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class NumeroPar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+      int contador = 0;
+      
+      while(contador < 100) {
+        if(contador%2==0){
+          System.out.println("es par: "+contador);
+                   
+        }
+        contador++;
+      }
+  }
+}
+      
+      
+    
+    
+
